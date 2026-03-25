085dcd106085ca29a882a5aa367387ed0479f394
Márcio Pereira de Deus <seuemail@gmail.com>
2026-03-25 12:27:08 +0000

    total de vendas sendo puxado do banco

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -5,6 +5,8 @@ namespace App\Http\Controllers;
 use App\Models\Venda;
 use App\Models\Cliente;
 use App\Models\Produto;
+use App\Models\ItemVenda;
+
 
 class DashboardController extends Controller
 {
@@ -18,15 +20,16 @@ class DashboardController extends Controller
         return view('dashboard.produtos', compact('produtos'));
     }
 
-public function index()
-{
-    $totalClientes = Cliente::count();
-    $totalVendas = Venda::count();
+    public function index()
+    {
+        $totalClientes = Cliente::count();
 
-    return view('dashboard', compact(
-        'totalClientes',
-        'totalVendas'
-    ));
-}
+        // 👇 ALTERAÇÃO AQUI
+        $totalVendas = ItemVenda::sum('subtotal');
 
-}
+        return view('dashboard', compact(
+            'totalClientes',
+            'totalVendas'
+        ));
+    }
+}               
